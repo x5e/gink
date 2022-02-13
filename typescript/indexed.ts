1e3d87cef56d494ac3eff774f82969f6586d5dcd
Darin McGill <darin.mcgill@gmail.com>
2022-02-13 01:28:47 -0500

    getNeededTransactions (draft)

--- typescript/indexed.ts
+++ typescript/indexed.ts
@@ -7,11 +7,16 @@ if (eval("typeof indexedDB") == 'undefined') {  // ts-node has problems with typ
 }
 import { Transaction } from "transactions_pb";
 import { Greeting, Log as TransactionLog } from "messages_pb";
-import { openDB, deleteDB, IDBPDatabase, IDBPTransaction } from 'idb';
+import { openDB, IDBPDatabase, IDBPTransaction } from 'idb';
 
 type Medallion = number;
 type Timestamp = number;
 type ChainStart = Timestamp;
+type PriorTime = Timestamp;
+type SeenThrough = Timestamp;
+type HasMap = Map<Medallion,Map<ChainStart,SeenThrough>>;
+type GinkTrxnBytes = Uint8Array;
+type GreetingBytes = Uint8Array;
 
 interface ChainInfo {
     medallion: Medallion;
@@ -20,14 +25,20 @@ interface ChainInfo {
     lastComment?: string;
 }
 
+// [Timestamp, Medallion] should enough to uniquely specify a Transaction.
+// ChainStart and PriorTime are just included here to avoid re-parsing.
+type TransactionKey = [Timestamp, Medallion, ChainStart, PriorTime];
+
 interface FsHandle {
     appendFile(uint8Array: Uint8Array): Promise<any>;
     close(): void;
 }
 
 export class IndexedGink {
+
     #pWrapped: Promise<IDBPDatabase>;
     fileHandle: FsHandle|null = null;
+
     constructor(
         indexedDbName = "gink",
     ) {
@@ -50,6 +61,7 @@ export class IndexedGink {
             },
         });
     }
+
     async close() {
         const db = await this.#pWrapped;
         db.close();
@@ -57,7 +69,8 @@ export class IndexedGink {
             this.fileHandle.close();
         }
     }
-    static async withTransactionLog(fn: string): Promise<IndexedGink> {
+
+    static async withTransactionLog(filename: string): Promise<IndexedGink> {
         /*
             The current implementation uses a fake in-memory implementation
             of IndexedDB.  So rather than rely on that for durability of 
@@ -67,7 +80,7 @@ export class IndexedGink {
             durable indexedDB implementation (when I can find/write one).
         */
         // TODO: probably should get an exclusive lock on the file
-        const fileHandle = await promises.open(fn, "a+");
+        const fileHandle = await promises.open(filename, "a+");
         const stats = await fileHandle.stat();
         const size = stats.size;
         const uint8Array = new Uint8Array(size);
@@ -82,7 +95,8 @@ export class IndexedGink {
         indexedGink.fileHandle = fileHandle;
         return indexedGink;
     }
-    async getGreeting(): Promise<Uint8Array> {
+    
+    async getGreeting(): Promise<GreetingBytes> {
         const asEntries = (await this.#getChainInfos()).map((value) => {
             let entry = new Greeting.GreetingEntry();
             entry.setMedallion(value.medallion);
@@ -94,36 +108,39 @@ export class IndexedGink {
         greeting.setEntriesList(asEntries);
         return greeting.serializeBinary();
     }
+
     async #getChainInfos(): Promise<Array<ChainInfo>> {
         let db: IDBPDatabase = await this.#pWrapped;
         let wrappedTransaction: IDBPTransaction = db.transaction(['chainInfos']);
         let store = wrappedTransaction.objectStore('chainInfos');
         return await store.getAll();
     }
-    async addTransaction(trxn: Uint8Array) {
-        let deserialized = Transaction.deserializeBinary(trxn);
-        let infoKey = [deserialized.getMedallion(), deserialized.getChainStart()];
+
+    async addTransaction(trxn: GinkTrxnBytes) {
+        let parsed = Transaction.deserializeBinary(trxn);
+        let infoKey = [parsed.getMedallion(), parsed.getChainStart()];
         let db: IDBPDatabase = await this.#pWrapped;
         let wrappedTransaction = db.transaction(['trxns', 'chainInfos'], 'readwrite');
-        let previous = deserialized.getPreviousTimestamp();
-        let newTimestamp = deserialized.getTimestamp();
+        let previous = parsed.getPreviousTimestamp();
+        let newTimestamp = parsed.getTimestamp();
         let present: ChainInfo = await wrappedTransaction.objectStore("chainInfos").get(infoKey);
         if (present || previous != 0) {
             if (present?.seenThrough >= newTimestamp) {
                 return present;
             }
             if ( present?.seenThrough != previous) {
-                throw new Error(`missing prior chain entry for ${deserialized.toObject()}, have ${present}`);
+                throw new Error(`missing prior chain entry for ${parsed.toObject()}, have ${present}`);
             }
         }
         let newInfo: ChainInfo = {
-            medallion: deserialized.getMedallion(),
-            chainStart: deserialized.getChainStart(),
-            seenThrough: deserialized.getTimestamp(),
-            lastComment: deserialized.getComment(),
+            medallion: parsed.getMedallion(),
+            chainStart: parsed.getChainStart(),
+            seenThrough: parsed.getTimestamp(),
+            lastComment: parsed.getComment(),
         }
         await wrappedTransaction.objectStore("chainInfos").put(newInfo);
-        const trxnKey = [deserialized.getTimestamp(), deserialized.getMedallion()];
+        const trxnKey: TransactionKey = [parsed.getTimestamp(), parsed.getMedallion(), 
+            parsed.getChainStart(), parsed.getPreviousTimestamp()];
         await wrappedTransaction.objectStore("trxns").add(trxn, trxnKey);
         await wrappedTransaction.done;
         if (this.fileHandle) {
@@ -133,16 +150,57 @@ export class IndexedGink {
         }
         return newInfo;
     }
-    async getNeededTransactions(greeting: Uint8Array): Promise<Iterable<Uint8Array>> {
-        const parsed = Greeting.deserializeBinary(greeting);
-        const tree: Map<Medallion,Map<ChainStart,Greeting.GreetingEntry>> = new Map();
-        let entry: Greeting.GreetingEntry;
+
+    // Note the IndexedDB has problems when await is called on anything unrelated
+    // to the current transaction, so its best if `callBack` doesn't call await.
+    async getNeededTransactions(
+            callBack: (x: GinkTrxnBytes) => void, 
+            greeting: Uint8Array|null = null, 
+            partialOkay: boolean = false): Promise<HasMap> {
+        const parsed = greeting ? Greeting.deserializeBinary(greeting) : new Greeting();
+        const hasMap: HasMap = new Map();
+        let entry: Greeting.GreetingEntry|null;
         for (entry in parsed.getEntriesList()) {
-            if (!tree.has(entry.getMedallion())) {
-                tree.set(entry.getMedallion(), new Map());
+            if (!hasMap.has(entry.getMedallion())) {
+                hasMap.set(entry.getMedallion(), new Map());
             }
-            tree.get(entry.getMedallion()).set(entry.getChainStart(), entry);
+            hasMap.get(entry.getMedallion()).set(entry.getChainStart(), entry.getSeenThrough());
         }
-        return [];
+        const db = await this.#pWrapped;
+        
+        for (let cursor = await db.transaction("trxns").objectStore("trxns").openCursor(); 
+            cursor; cursor = await cursor.continue()) {
+            const key = <TransactionKey> cursor.key;
+            const ginkTrxn: GinkTrxnBytes = cursor.value;
+            const [trxnTime, medallion, chainStart, priorTime] = key;
+            if (!hasMap.has(medallion)) {hasMap.set(medallion, new Map());}
+            let seenThrough = hasMap.get(medallion).get(chainStart);
+            if (!seenThrough) {
+                if (priorTime == 0 || partialOkay) {
+                    // happy path: sending the start of a chain
+                    callBack(ginkTrxn);
+                    hasMap.get(medallion).set(chainStart, trxnTime);
+                    continue;
+                } 
+                throw new Error(`Peer doesn't have the start of ${medallion},${chainStart}` +
+                    "and neither do I and partialOkay=False"); 
+            } 
+            if (seenThrough >= trxnTime) {
+                continue;  // happy path: peer doesn't need this transaction
+            }
+            if (seenThrough == priorTime) {
+                // another happy path: peer has everything in this chain up to this transaction
+                callBack(ginkTrxn);
+                hasMap.get(medallion).set(chainStart, trxnTime);
+                continue;
+            }
+            if (partialOkay) {
+                // not really a happy path but we're just going to skip syncing this chain
+                continue;
+            }
+            throw new Error(`unable to continue chain ${medallion},${chainStart} ` +
+                `peer has seenThrough=${seenThrough}, I have priorTime=${priorTime}`);
+          }
+        return hasMap;
     }
 }
