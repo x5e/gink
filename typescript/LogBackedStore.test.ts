4619a23edabb514971f312384f2a433f0e63f610
Darin McGill <darin.mcgill@gmail.com>
2022-03-27 02:25:36 -0400

    added test for locking

--- typescript/LogBackedStore.test.ts
+++ typescript/LogBackedStore.test.ts
@@ -1,4 +1,16 @@
-import {LogBackedStore} from "./LogBackedStore";
+import { LogBackedStore } from "./LogBackedStore";
 import { testStore } from "./Store.test";
 
 testStore('LogBackedStore', async () => new LogBackedStore("/tmp/test.commits", true));
+
+test('test locks', async () => {
+    const fn = "/tmp/test.store";
+    const lbs1 = new LogBackedStore(fn);
+    await lbs1.initialized;
+    let result = "undecided";
+    const lbs2 = new LogBackedStore(fn);
+    await lbs2.initialized.then(() => {result="acquired";}).catch(() => {result="barfed";});
+    if (result != "barfed") {
+        throw new Error("locking broken");
+    }
+})
