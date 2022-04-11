f67513db072a2e54ea7b7e1c64e2e10cdc20f1bc
Darin McGill <darin.mcgill@gmail.com>
2022-04-11 02:23:15 -0400

    implementation of addCommit

--- typescript/Client.ts
+++ typescript/Client.ts
@@ -3,9 +3,10 @@ var W3cWebSocket = typeof WebSocket == 'function' ? WebSocket :
 import { Peer } from "./Peer";
 import { Store } from "./Store";
 import { makeHasMap, hasMapToGreeting, makeMedallion, assert } from "./utils";
-import { HasMap, CommitBytes, CommitInfo, ClaimedChains, Medallion, ChainStart, Timestamp } from "./typedefs";
+import { HasMap, CommitBytes, CommitInfo, ClaimedChains, Medallion, ChainStart, Timestamp, Offset } 
+    from "./typedefs";
 import { Message } from "messages_pb";
-import { Commit } from "transactions_pb";
+import { Commit as CommitMessage } from "transactions_pb";
 
 
 export class Client {
@@ -22,6 +23,10 @@ export class Client {
         this.initialized = this.#initialize();
     }
 
+    /**
+     * 
+     * @returns Promise of a new chain manager that can be used to create new commits.
+     */
     async getChainManager(): Promise<ChainManager> {
         let medallion: number;
         let chainStart: number;
@@ -29,7 +34,7 @@ export class Client {
         if (this.#availableChains.size == 0) {
             medallion = makeMedallion();
             seenTo = chainStart = Date.now() * 1000;
-            const startCommit = new Commit();
+            const startCommit = new CommitMessage();
             startCommit.setTimestamp(seenTo);
             startCommit.setChainStart(chainStart);
             startCommit.setMedallion(medallion);
@@ -156,11 +161,96 @@ export class ChainManager {
     readonly #client: Client;
     readonly #medallion: Medallion;
     readonly #chainStart: ChainStart;
-    #lastSeen: Timestamp;
+    #last: Promise<Timestamp>;
     constructor(client: Client, medallion: Medallion, chainStart: ChainStart, lastSeen: Timestamp) {
         this.#client = client;
         this.#medallion = medallion;
         this.#chainStart = chainStart;
-        this.#lastSeen = lastSeen;
+        this.#last = new Promise((resolve, _reject) => {resolve(lastSeen)});
+    }
+
+    get medallion() { return this.#medallion; }
+    
+    /**
+     * Adds a commit to a chain, setting the medallion and timestamps on the commit in the process.
+     * 
+     * @param commit 
+     * @returns A promise that will resolve to the commit timestamp once it's persisted/sent.
+     */
+    async addCommit(commit: Commit): Promise<Timestamp> {
+        // We want to ensure that commits are ordered on the chain in the order that addCommit is called.
+        this.#last = this.#last.then((lastTimestamp) => new Promise<number>((resolve)=> {
+            // If the current time isn't greater than the last timestamp, then we need to wait a bit 
+            // so that all commits get a unique timestamp.
+            const waitNeeded = Date.now() * 1000 > lastTimestamp ? 0 : 1;
+            setTimeout(async () => {
+                const newTimestamp = Date.now() * 1000;
+                assert(newTimestamp > lastTimestamp);
+                const bytes = commit.seal(this.#medallion, this.#chainStart, lastTimestamp, newTimestamp);
+                await this.#client.receiveCommit(undefined, bytes);
+                resolve(newTimestamp);
+            }, waitNeeded);
+        }));
+        return this.#last;
+    }
+}
+
+/**
+ * An open transaction that you can add objects to.  It's a little funky because the timestamp
+ * of the commit will be determined when it's closed, so the ID of any object added to the commit
+ * isn't completely known until after it's closed.  (That's required to avoid objects referencing 
+ * other objects with timestamps in the future).
+ */
+export class Commit {
+    #comment: string|null = null;
+    #timestamp: Timestamp|null = null;
+    #medallion: Medallion|null = null;
+
+    constructor(comment?: string) {
+        this.#comment = comment;
+    }
+
+    set comment(value: string) {
+        assert(!this.#timestamp); 
+        this.#comment = value; 
+    }
+
+    addObj(_obj: Obj): Identifier {
+        throw new Error("not implemented");
+    }
+
+    seal(medallion: Medallion, chainStart: ChainStart, priorTimestamp: Timestamp, timestamp: Timestamp) {
+        assert(!this.#timestamp);
+        this.#timestamp = timestamp;
+        this.#medallion = medallion;
+        const commitMessage = new CommitMessage();
+        commitMessage.setTimestamp(timestamp);
+        commitMessage.setPreviousTimestamp(priorTimestamp);
+        commitMessage.setChainStart(chainStart);
+        commitMessage.setMedallion(medallion);
+        if (this.#comment) { commitMessage.setComment(this.#comment); }
+        return commitMessage.serializeBinary();
+    }
+
+    get timestamp() {
+        assert(this.#timestamp);
+        return this.#timestamp;
+    }
+
+    get medallion() {
+        assert(this.#medallion);
+        return this.#medallion;
+    }
+
+}
+
+export class Obj {}
+
+export class Identifier {
+    readonly commit: Commit;
+    readonly offset: Offset;
+    constructor(commit: Commit, offset: Offset) {
+        this.offset = offset;
+        this.commit = commit;
     }
 }
--- typescript/typedefs.ts
+++ typescript/typedefs.ts
@@ -7,7 +7,7 @@ export type SeenThrough = Timestamp;
 export type HasMap = Map<Medallion,Map<ChainStart,SeenThrough>>;
 export type PriorTime = Timestamp;
 export type ClaimedChains = Map<Medallion, ChainStart>;
-
+export type Offset = number;
 
 // [Timestamp, Medallion] should enough to uniquely specify a Commit.
 // ChainStart and PriorTime are just included here to avoid re-parsing.
