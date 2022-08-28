185f448ee57818e6fdebab280969014ec6d3c4d2
Darin McGill <darinmcgill@google.com>
2022-08-28 02:09:44 -0400

    tweaks

--- typescript/integration-test.js
+++ typescript/integration-test.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --unhandled-rejections=strict
+#!/usr/bin/env -S node --unhandled-rejections=strict
 const Expector = require("./Expector");
 (async () => {
     console.log("starting");
