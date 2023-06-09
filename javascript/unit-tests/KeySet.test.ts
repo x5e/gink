67d2a8bfd0787c3d1885d17c91c51b484740e00c
char26 <charliealders26@gmail.com>
2023-06-09 11:57:33 -0700

    finished writing tests for ts keyset

--- javascript/unit-tests/KeySet.test.ts
+++ javascript/unit-tests/KeySet.test.ts
@@ -1,6 +1,6 @@
 
 import { sleep } from "./test_utils";
-import { GinkInstance, Bundler, IndexedDbStore, KeySet } from "../implementation";
+import { GinkInstance, Bundler, IndexedDbStore } from "../implementation";
 import { ensure } from "../implementation/utils"
 
 test('add and has basic data', async function() {
@@ -21,7 +21,6 @@ test('add and has basic data', async function() {
 
     await ks.add(myKey);
     ensure(await ks.has(myKey));
-
 });
 
 test('delete and size work as intended', async function() {
@@ -39,7 +38,6 @@ test('delete and size work as intended', async function() {
     await ks.add("key1");
     await ks.add("key2");
     ensure(await ks.size() === 2);
-
 });
 
 test('update and entries work as intended', async function() {
@@ -59,7 +57,6 @@ test('update and entries work as intended', async function() {
     const myMap2 = await ks.entries();
     ensure(myMap2.has("key6"));
     ensure(!myMap2.has("key5"));
-
 });
 
 test('tests keys and values both return the correct set', async function() {
@@ -79,7 +76,6 @@ test('tests keys and values both return the correct set', async function() {
     ensure(keys.size === 3);
     ensure(keys.has("key1"));
     ensure(keys.has("key3"));
-
 });
 
 test('tests superset, union, intersection, symmetric difference, difference', async function() {
@@ -116,7 +112,6 @@ test('tests superset, union, intersection, symmetric difference, difference', as
     ensure(difference.has("key1"));
     ensure(difference.has("key2"));
     ensure(!difference.has("key3"));
-
 });
 
 test('KeySet.toJson', async function() {
@@ -128,7 +123,6 @@ test('KeySet.toJson', async function() {
     
     const asJSON = await ks.toJson();
     ensure(asJSON == `{"key1","key2","key3"}`, asJSON);
-
 });
 
 test('KeySet.asOf', async function() {
@@ -161,15 +155,58 @@ test('KeySet.asOf', async function() {
     ensure(await ks.size(time2)==2);
 
     // testing asOf for entries, values, and keys
-    const values = await ks.values(time0)
-    const keys = await ks.keys(time1)
-    const entries = await ks.entries(time2)
+    const values = await ks.values(time0);
+    const keys = await ks.keys(time1);
+    const entries = await ks.entries(time2);
 
-    ensure(!values.size)
-    ensure(keys.size==1)
-    ensure(entries.size==2)
-    ensure(keys.has("key1") && entries.has("key1"))
-    ensure(!keys.has("key2"))
+    ensure(!values.size);
+    ensure(keys.size==1);
+    ensure(entries.size==2);
+    ensure(keys.has("key1") && entries.has("key1"));
+    ensure(!keys.has("key2"));
+});
 
+test('KeySet.clear', async function() {
+    const instance = new GinkInstance(new IndexedDbStore('test8', true));
+    const ks = await instance.createKeySet();
+    await ks.update(["key1", "key2"]);
+    const clearMuid = await ks.clear();
+    await ks.update(["key3", "key4"]);
+    const asMap = await ks.entries();
+    ensure(asMap.has("key4") && !asMap.has("key1"), "did not clear")
+    const asMapBeforeClear = await ks.entries(clearMuid.timestamp);
+    if (asMapBeforeClear.has("key4") || !asMapBeforeClear.has("key1")) {
+        console.log(asMapBeforeClear);
+        throw new Error("busted");
+    }
+});
 
+test('KeySet.purge', async function () {
+    const instance = new GinkInstance(new IndexedDbStore('test9', true));
+    const ks = await instance.createKeySet();
+    await ks.add('key1');
+    await ks.add('key2');
+    let size = await ks.size();
+    ensure(size == 2);
+    await ks.clear(true);
+    size = await ks.size();
+    ensure(size == 0);
+});
+
+test('add multiple keys within a bundler', async function() {
+    const store = new IndexedDbStore('test10', true);
+    const instance = new GinkInstance(store);
+    const ks = await instance.createKeySet();
+
+    // make multiple changes in a change set
+    const bundler = new Bundler();
+    await ks.add("key1", bundler);
+    await ks.add("key2", bundler);
+    bundler.comment = "My first bundle!";
+    await instance.addBundler(bundler);
+
+    // verify the result
+    ensure(await ks.has("key1"));
+    ensure(await ks.has("key2"));
+    ensure(!await ks.has("key3"));
 });
