aec3a3b01cc21ff14215339af4fd8e1838a699ec
char26 <charliealders26@gmail.com>
2023-07-02 11:14:10 -0700

    added a couple more asof tests

--- javascript/unit-tests/PairSet.test.ts
+++ javascript/unit-tests/PairSet.test.ts
@@ -1,5 +1,6 @@
 import { GinkInstance, IndexedDbStore } from "../implementation";
 import { ensure, muidToString, muidTupleToMuid, stringToMuid } from "../implementation/utils";
+import { sleep } from "./test_utils";
 
 test('include, exclude, and contains work as intended', async function() {
     const store = new IndexedDbStore('test1', true);
@@ -31,12 +32,24 @@ test('asOf and get_pairs work properly', async function() {
     const box3 = await instance.createBox();
 
     await ps1.include([box1, box2]);
+    await sleep(10);
+    const time0 = Date.now() * 1000;
+    await sleep(10);
     await ps1.include([box2, box3]);
+    await sleep(10);
     const time1 = Date.now() * 1000;
+    await sleep(10);
     await ps1.include([box1, box3]);
+    await sleep(10);
     ensure(await ps1.contains([box1, box2], time1));
     ensure(!await ps1.contains([box1, box3], time1));
 
     const toSet = await ps1.get_pairs();
     ensure(toSet.size == 3);
+
+    const asOfSet = await ps1.get_pairs(time0);
+    ensure(asOfSet.size == 1);
+
+    ensure(await ps1.size(time0)==1);
+    ensure(!await ps1.contains([box1, box3], time0));
 });
