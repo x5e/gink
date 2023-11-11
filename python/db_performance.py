83343c7681ef59365de27a51cbb840fd81f02be8
Charlie Alders <charliealders26@gmail.com>
2023-11-10 20:00:26 -0800

    simple read and write test working

--- python/db_performance.py
+++ python/db_performance.py
@@ -1,50 +1,57 @@
-from json import *
-from timeit import timeit
+from datetime import datetime
 from pathlib import Path
 from gink import *
 
-def generate_json(file_path: str, count: int):
+def test_write_fresh(count: int):
     """
-    Generates a JSON file with a size of 179MB if range is 500k
-    or 1.68GB if range is set to 5 mil
+    Test writes per second writing to an empty database.
     """
-    with open(file_path, 'w') as file_handle:
-        test = {"test": {}}
-        for i in range(0, count):
-            test['test'][f'test{i}'] = {
-                "testData1": "14239081741847918471481",
-                "testData2": "14239081741847918471481",
-                "testData3": "14239081741847918471481",
-                "testData4": "14239081741847918471481",
-                "testData5": "14239081741847918471481",
-                "testData6": "14239081741847918471481",
-                "testData7": "14239081741847918471481",
-                "testData8": "14239081741847918471481",
-                "testData9": "14239081741847918471481"
-                }
-        dump(test, file_handle, indent=1)
-        del test
+    directory = Directory(muid=Muid(1, 2, 3))
+    before_time = datetime.utcnow()
+    print("Testing Gink writing performance to fresh database")
+    print("Writing", count, "entries.")
+    for i in range(0, count):
+        directory.set(f"test{i}", "test data to be inserted")
+    after_time = datetime.utcnow()
 
-def insert_json(path_to_json_file: Path, path_to_gink_db: Path) -> int:
-    with open(path_to_json_file, 'r') as f:
-        file = f.read()
-    store = LmdbStore(path_to_gink_db)
-    db = Database(store)
-    directory = Directory(db, contents=loads(file))
-    return directory.size()
+    total_time = round((after_time - before_time).total_seconds(), 4)
+    writes_per_second = count/total_time
+    print("- Total time:", total_time, "seconds")
+    print("- Writes per second:", round(writes_per_second, 2))
 
-def test_db(path_to_json_file: Path, path_to_gink_db: Path, count: int):
-    generate_json(path_to_json_file, count)
-    print("file populated")
-    inserted = insert_json(path_to_json_file, path_to_gink_db=path_to_gink_db)
-    print(f"done, inserted: {inserted}")
-    #print("done!\ntook", timeit("insert_json('large_file.json')", "from __main__ import insert_json", number=1), " seconds.")
+def test_read(count: int):
+    """
+    Tests reads per second, or how long it takes to read
+    all entries populated by the write test.
+    """
+    directory = Directory(muid=Muid(1, 2, 3))
+    print("Testing Gink reading performance")
+    print("Reading", count, "entries.")
+    before_time = datetime.utcnow()
+    for i in range(0, count):
+        assert directory.get(f"test{i}")
+    after_time = datetime.utcnow()
+
+    total_time = round((after_time - before_time).total_seconds(), 4)
+    reads_per_second = count/total_time
+    print("- Total time:", total_time, "seconds")
+    print("- Reads per second:", round(reads_per_second, 2))
+    
+def test_db(count: int):
+    test_write_fresh(count)
+    print()
+    test_read(count)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser, Namespace
+    
     parser: ArgumentParser = ArgumentParser(allow_abbrev=False)
     parser.add_argument("-c", "--count", help="number of records", type=int, default=10)
-    parser.add_argument("-j", "--json", help="path to json", default="test.json")
     parser.add_argument("-g", "--gink", help="path to gink db", default="gink.db")
     args: Namespace = parser.parse_args()
-    test_db(path_to_json_file=args.json, path_to_gink_db=args.gink, count=args.count)
+
+    with LmdbStore(args.gink) as store:
+        db = Database(store)
+        # I'm setting the database and store here because the directories
+        # will use Database.get_last().
+        test_db(count=args.count)
