75db887495856d29e656a28ec0c8292771d790d5
Charlie Alders <charliealders26@gmail.com>
2023-11-16 13:19:26 -0800

    y-axis spacing isn't blocking bar anymore

--- python/gink/performance_tests/comparison_graph.py
+++ python/gink/performance_tests/comparison_graph.py
@@ -4,6 +4,13 @@ import json
 import numpy as np
 
 def graph_write(path_to_data: Path):
+    """
+    Plots a bar chart for writing to fresh db with individual commits
+    writing to a fresh db with one big commit, and writing to an occupied db.
+    
+    This doesn't call plt.show(). Do that after creating as
+    many of the graphs as necessary.
+    """
     with open(path_to_data, "r") as f:
         data: dict = json.loads(f.read())
     assert data
@@ -20,11 +27,18 @@ def graph_write(path_to_data: Path):
     }
     plt.figure(1)
     ax = plt.subplot()
+    y_max = 0
     for db in data.keys():
-        all_data["write_fresh"].append(data[db]["write_fresh"]["writes_per_second"])
-        all_data["write_occupied"].append(data[db]["write_occupied"]["writes_per_second"])
-        all_data["write_big_commit"].append(data[db]["write_big_commit"]["writes_per_second"])
+        wf = data[db]["write_fresh"]["writes_per_second"]
+        wo = data[db]["write_occupied"]["writes_per_second"]
+        wbc = data[db]["write_big_commit"]["writes_per_second"]
+        all_data["write_fresh"].append(wf)
+        all_data["write_occupied"].append(wo)
+        all_data["write_big_commit"].append(wbc)
     
+    if y_max < wf or y_max < wo or y_max < wbc:
+            y_max = max(wf, wo, wbc)
+
     for test, results in all_data.items():
         offset = width * multiplier
         rects = ax.bar(x + offset, results, width, label=test)
@@ -33,9 +47,16 @@ def graph_write(path_to_data: Path):
     ax.set_ylabel('Writes per second')
     ax.set_title('Writes per second - Gink vs SQLite')
     ax.set_xticks(x + width, x_labels)
+    ax.set_ylim(bottom=0, top=y_max+(y_max*.2))
     ax.legend(loc='upper left', ncols=3)
 
 def graph_read(path_to_data: Path):
+    """
+    Plots a bar chart for write/read, read, and random reads.
+    
+    This doesn't call plt.show(). Do that after creating as
+    many of the graphs as necessary.
+    """
     with open(path_to_data, "r") as f:
         data: dict = json.loads(f.read())
     assert data
@@ -52,10 +73,17 @@ def graph_read(path_to_data: Path):
     }
     plt.figure(2)
     ax = plt.subplot()
+    y_max = 0
     for db in data.keys():
-        all_data["read"].append(data[db]["read"]["reads_per_second"])
-        all_data["read_write"].append(data[db]["read_write"]["txns_per_second"])
-        all_data["random_read"].append(data[db]["random_read"]["reads_per_second"])
+        read_rps = data[db]["read"]["reads_per_second"]
+        wr_rps = data[db]["read_write"]["txns_per_second"]
+        rand_rps = data[db]["random_read"]["reads_per_second"]
+        all_data["read"].append(read_rps)
+        all_data["read_write"].append(wr_rps)
+        all_data["random_read"].append(rand_rps)
+
+        if y_max < read_rps or y_max < wr_rps or y_max < rand_rps:
+            y_max = max(read_rps, wr_rps, rand_rps)
     
     for test, results in all_data.items():
         offset = width * multiplier
@@ -65,38 +93,50 @@ def graph_read(path_to_data: Path):
     ax.set_ylabel('Reads per second')
     ax.set_title('Reads per second - Gink vs SQLite')
     ax.set_xticks(x + width, x_labels)
+    ax.set_ylim(bottom=0, top=y_max+(y_max*.2))
     ax.legend(loc='upper left', ncols=3)
 
 def graph_delete(path_to_data: Path):
+    """
+    Plots bar chart for deletions per second. 
+    
+    This doesn't call plt.show(). Do that after creating as
+    many of the graphs as necessary.
+    """
     with open(path_to_data, "r") as f:
         data: dict = json.loads(f.read())
     assert data
     x_labels = [label for label in data.keys()]
     x = np.arange(len(x_labels))
-    width = 0.25
-    multiplier = 0
+    width = 1/len(x_labels)
     all_data = {
         "delete": []
     }
     plt.figure(3)
     ax = plt.subplot()
+    y_max = 0
     for db in data.keys():
-        all_data["delete"].append(data[db]["delete"]["deletes_per_second"])
+        dps = data[db]["delete"]["deletes_per_second"]
+        all_data["delete"].append(dps)
+        if y_max < dps:
+            y_max = dps
     
-    for test, results in all_data.items():
-        offset = width * multiplier
-        rects = ax.bar(x + offset, results, width, label=test)
-        ax.bar_label(rects, padding=3)
-        multiplier += 1
-    ax.set_ylabel('Deletes per second')
-    ax.set_title('Deletes per second - Gink vs SQLite')
-    ax.set_xticks(x + width, x_labels)
-    ax.legend(loc='upper left', ncols=3)
+    for i in range(len(all_data["delete"])):
+        rects = ax.bar(x[i], all_data["delete"][i], width, label=x_labels[i])
+
+    ax.bar_label(rects)
+    ax.set_ylabel('Deletions per second')
+    ax.set_title('Deletions per second - Gink vs SQLite')
+    ax.set_ylim(bottom=0, top=y_max+(y_max*.1))
+    ax.set_xticks(x, x_labels)
 
 def graph_increasing(path_to_data: Path):
     """
     Plots line graphs for both read and write as 
     data increases.
+
+    This doesn't call plt.show(). Do that after creating as
+    many of the graphs as necessary.
     """
     with open(path_to_data, "r") as f:
         data: dict = json.loads(f.read())
@@ -105,14 +145,23 @@ def graph_increasing(path_to_data: Path):
     plt.figure(4, figsize=(10, 10))
     ax1 = plt.subplot(211)
     ax2 = plt.subplot(212)
+    y_max_w = 0
+    y_max_r = 0
     for db in data.keys():
         x = []
         writes = []
         reads = []
         for key in data[db]["increases"].keys():
             x.append(key)
-            writes.append(data[db]["increases"][key]["write"]["writes_per_second"])
-            reads.append(data[db]["increases"][key]["read"]["reads_per_second"])
+            wps = data[db]["increases"][key]["write"]["writes_per_second"]
+            rps = data[db]["increases"][key]["read"]["reads_per_second"]
+            writes.append(wps)
+            reads.append(rps)
+            if y_max_w < wps:
+                y_max_w = wps
+            if y_max_r < rps:
+                y_max_r = rps
+
         ax1.plot(x, writes)
         ax2.plot(x, reads)
         legend.append(db)
@@ -120,13 +169,15 @@ def graph_increasing(path_to_data: Path):
     # All databases SHOULD have the same number of increasing tests,
     # so it should be fine to use the last db as the number of bins.
     num_bins = len(data[db]["increases"].keys())
+
     ax1.set_title("Writes per second as DB Increases")
     ax1.set_xlabel("# of Entries in Database")
     ax1.set_ylabel("Writes per second")
+    ax2.set_ylim(bottom=0, top=y_max_r+(y_max_r*.1))
     ax2.set_title("Reads per second as DB Increases")
     ax2.set_xlabel("# of Entries in Database")
     ax2.set_ylabel("Reads per second")
-    ax2.set_ylim(bottom=0)
+    ax2.set_ylim(bottom=0, top=y_max_r+(y_max_r*.1))
     ax1.xaxis.set_major_locator(plt.MaxNLocator(num_bins))
     ax2.xaxis.set_major_locator(plt.MaxNLocator(num_bins))
     ax1.legend(legend, ncol=2)
