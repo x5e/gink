c8975ce414ec05b61c0e87b8576e83475b953bfd
char26 <charliealders26@gmail.com>
2023-12-29 11:11:24 -0800

    javascript rendering based on hash

--- javascript/static/dashboard/scripts/display_container.js
+++ javascript/static/dashboard/scripts/display_container.js
@@ -1,10 +1,35 @@
-async function displayContainer(muidString) {
+/**
+ * Edits the HTML to display the contents of a container.
+ * Can take either the Muid object itself, or the canonical
+ * string muid.
+ * @param {String | Muid} muid
+ */
+async function displayContainer(muid) {
     const containerContents = document.getElementById('container-contents');
-    const thisContainerDiv = containerContents.appendChild(document.createElement('div'));
-    thisContainerDiv.setAttribute('class', 'container-box');
-    const container = await gink.construct(window.instance, gink.strToMuid(muidString));
-    for (const [key, val] of container.toMap().entries()) {
-        const p = thisContainerDiv.appendChild(document.createElement("p"));
-        p.innerText = key + " - " + val;
+    let container;
+    if (typeof muid == "string") {
+        muid = gink.strToMuid(muid);
     }
+    try {
+        container = await gink.construct(window.instance, muid);
+        writeTitle(container);
+        const thisContainerDiv = containerContents.appendChild(document.createElement('div'));
+        thisContainerDiv.setAttribute('class', 'container-box');
+        const asMap = await container.toMap();
+        for (const [key, val] of asMap.entries()) {
+            const p = thisContainerDiv.appendChild(document.createElement("p"));
+            p.innerText = key + " -> " + val;
+        }
+    } catch (e) {
+        const p = containerContents.appendChild(document.createElement('p'));
+        p.innerText = "Container not found.";
+    }
+}
+
+function writeTitle(container) {
+    const containerContents = document.getElementById('container-contents');
+    const titleBar = containerContents.appendChild(document.createElement('div'));
+    titleBar.setAttribute('id', 'title-bar');
+    const muid = container.address;
+    titleBar.innerText = `${container.constructor.name} (${muid.timestamp},${muid.medallion},${muid.offset})`;
 }
