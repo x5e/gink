d7d72393c4c6adc364b3a62d7aea3b8e29e55fb6
Charlie Alders <charliealders26@gmail.com>
2024-01-06 13:50:43 -0800

    added getAll to property

--- javascript/unit-tests/Property.test.ts
+++ javascript/unit-tests/Property.test.ts
@@ -1,5 +1,5 @@
 import { GinkInstance, IndexedDbStore, MemoryStore } from "../implementation";
-import { ensure, sameData } from "../implementation/utils";
+import { ensure, muidToString, sameData } from "../implementation/utils";
 
 it('Property.basics', async function () {
     for (const store of [new IndexedDbStore('Property.basics', true), new MemoryStore(true)]) {
@@ -14,6 +14,11 @@ it('Property.basics', async function () {
         await property.set(gp, [1, 2, 3]);
         const gotten2 = await property.get(gp);
         ensure(sameData(gotten2, [1, 2, 3]));
+
+        const allEntries = await property.getAll();
+        ensure(allEntries.get(muidToString(gd.address)).value == "foobar");
+        ensure(allEntries.get(muidToString(gp.address)).value.toString() == "1,2,3");
+
         const clearMuid = await property.clear();
         const hasGp = await property.has(gd);
         ensure(hasGp === false);
