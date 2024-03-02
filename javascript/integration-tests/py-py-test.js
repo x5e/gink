19db2d93f0103f81efa50e08a4d299d57c597a5d
Charlie Alders <charliealders26@gmail.com>
2024-03-02 12:39:29 -0800

    added python to python test

---
+++ javascript/integration-tests/py-py-test.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S node --unhandled-rejections=strict
+const Expector = require("./Expector.js");
+const { sleep } = require("./browser_test_utilities.js");
+
+(async () => {
+    console.log("starting");
+    const server = new Expector(
+        "python3",
+        ["-u", "-m", "gink", "-l"],
+        { env: { PYTHONPATH: "./python" } });
+    await server.expect("listen");
+
+    const client = new Expector(
+        "python3",
+        ["-u", "-m", "gink", "-c", "ws://localhost:8080"],
+        { env: { PYTHONPATH: "./python" } });
+    await client.expect("connect");
+    await server.expect("accepted");
+
+    server.send("Directory(root=True).set('3','4');\n");
+    await server.expect("Muid", 1000);
+    await sleep(100);
+
+    client.send("root.get('3');\n");
+    await client.expect("4", 1000);
+    await sleep(100);
+
+    await client.close();
+    await server.close();
+    console.log("finished!");
+    process.exit(0);
+})().catch((reason) => { console.error(reason); process.exit(1); });
+
+
