d60e8607ae8b28fbf686e64597a11e2084967379
Charlie Alders <charliealders26@gmail.com>
2024-03-04 21:37:25 -0800

    tweaked test to spawn ts process

--- javascript/integration-tests/ts-py-test.js
+++ javascript/integration-tests/ts-py-test.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S node --unhandled-rejections=strict
 const Expector = require("./Expector.js");
-const { GinkInstance } = require("../tsc.out/implementation/GinkInstance.js");
-const { ensure } = require("../tsc.out/implementation/utils.js");
+const { sleep } = require("./browser_test_utilities.js");
 
 (async () => {
     console.log("starting");
@@ -9,15 +8,19 @@ const { ensure } = require("../tsc.out/implementation/utils.js");
         "python3",
         ["-u", "-m", "gink", "-l", "*:8085"]);
     await python.expect("listen");
+    await sleep(100);
 
-    const client = new GinkInstance();
-    await client.connectTo("ws://localhost:8085");
+    const client = new Expector("./tsc.out/implementation/main.js", ["ws://localhost:8085"], { env: { ...process.env } });
+    await client.expect("starting...");
 
     python.send("Directory(root=True).set(3,4);\n");
     await python.expect("Muid", 1000);
 
-    ensure(await client.getGlobalDirectory().get(3) === 4);
+    await sleep(100);
+    client.send("await root.get(3);\n");
+    await client.expect("\n4\n");
 
+    await client.close();
     await python.close();
     console.log("finished!");
     process.exit(0);
