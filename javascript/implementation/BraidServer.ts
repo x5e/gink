6b4419bc6ac256915f12c2def6ad462849ebf45b
Darin McGill <darin.mcgill@gmail.com>
2024-04-07 01:33:23 -0400

    tweaks

---
+++ javascript/implementation/BraidServer.ts
@@ -0,0 +1,8 @@
+import { LockableLog } from "./LockableLog";
+import { LogBackedStore } from "./LogBackedStore";
+
+class BraidServer {
+    constructor(
+        private directory: string
+    ) {}
+}
