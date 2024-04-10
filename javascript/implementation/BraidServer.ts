aeecf8727495df8d148ef58827f4ccd4584ff35c
Darin McGill <darin.mcgill@gmail.com>
2024-04-10 19:08:12 -0400

    tweaks

--- javascript/implementation/BraidServer.ts
+++ javascript/implementation/BraidServer.ts
@@ -1,8 +1,15 @@
 import { LockableLog } from "./LockableLog";
 import { LogBackedStore } from "./LogBackedStore";
+import { join, resolve } from "path";
+import { ensure } from "./utils";
+import { SimpleServer } from "./SimpleServer";
 
 class BraidServer {
-    constructor(
-        private directory: string
-    ) {}
+    readonly directory: string;
+    static readonly EXTENSION = ".binlog"
+
+    constructor(readonly metadataSimpleServer: SimpleServer, directory: string) {
+        this.directory = resolve(directory);
+
+    }
 }
