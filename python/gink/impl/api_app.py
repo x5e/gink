320cd2472bd136fbc54e444e7462057bc32a5d8c
char26 <charliealders26@gmail.com>
2024-07-05 11:17:08 -0700

    working aside from types

--- python/gink/impl/api_app.py
+++ python/gink/impl/api_app.py
@@ -15,14 +15,14 @@ class ApiApp():
         if not raw_path or raw_path == '/':
             return self._bad_path_handler(start_response)
 
-        print(environ)
-
         request_body = environ.get("wsgi.input").read().decode("utf-8")
         d = parse_qs(request_body)
 
         if environ.get("REQUEST_METHOD") == "GET":
             default = object()
             result = self.root.get(raw_path.split("/"), default)
+            if result is default:
+                return self._data_not_found_handler(start_response)
             return self._response_handler(result, start_response)
         elif environ.get("REQUEST_METHOD") == "POST":
             try:
@@ -43,16 +43,28 @@ class ApiApp():
         start_response(status, headers)
         return [bytes(data)]
 
-    def _bad_path_handler(self, start_response):
+    def _data_not_found_handler(self, start_response):
         status = '404 Not Found'
         headers = [('Content-type', 'text/plain')]
         start_response(status, headers)
-        return [b'Please specify a directory path.']
+        return [b'Entry not found.']
+
+    def _bad_path_handler(self, start_response):
+        status = '400 Bad Request'
+        headers = [('Content-type', 'text/plain')]
+        start_response(status, headers)
+        return [b'Please specify a directory/key path.']
 
     def _bad_body_handler(self, start_response):
         status = '400 Bad Request'
         headers = [('Content-type', 'text/plain')]
         start_response(status, headers)
-        return [b'Please specify key and value in the body to post to the directory. Ensure key is present to get a value.']
+        return [b'Please specify value in the body to post to the directory.']
+
+    def _bad_method_handler(self, start_response):
+        status = '405 Method Not Allowed'
+        headers = [('Content-type', 'text/plain')]
+        start_response(status, headers)
+        return [b'The Gink API only supports GET and POST.']
 
 
