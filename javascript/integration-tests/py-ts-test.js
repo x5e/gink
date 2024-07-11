f3e44f2f57e7f26f2dc3c8b55d9e45e44283f758
char26 <charliealders26@gmail.com>
2024-07-11 11:54:17 -0700

    py-ts test tests datetime sending

--- javascript/integration-tests/py-ts-test.js
+++ javascript/integration-tests/py-ts-test.js
@@ -20,9 +20,26 @@ process.chdir(__dirname + "/..");
     await server.expect("received bundle", 1000);
     await sleep(100);
 
+    server.send("await root.set('date', new Date());\n");
+    await server.expect("received bundle", 1000);
+    await sleep(100);
+
     client.send("root.get(3);\n");
     await client.expect("\n4\n", 1000);
     await sleep(100);
+
+    client.send("root.get('date');\n");
+    await client.expect("datetime.datetime", 1000);
+    await sleep(100);
+
+    client.send("from datetime import datetime;\n");
+    await sleep(100);
+    client.send("root.set('date2', datetime.strptime('2024-07-11T11:45:34.319Z', '%Y-%m-%dT%H:%M:%S.%fZ'));\n");
+    await sleep(100);
+
+    server.send("await root.get('date2');\n");
+    await sleep(100);
+    await server.expect("2024-07-11T11:45:34.319Z", 1000);
 
     await client.close();
     await server.close();
