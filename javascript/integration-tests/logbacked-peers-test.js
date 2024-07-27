9121fd1ea3e0b644bc646d6c1a77b8c4da8174bc
Darin McGill <darin.mcgill@gmail.com>
2024-07-27 13:59:34 -0400

    tweaks

--- javascript/integration-tests/logbacked-peers-test.js
+++ javascript/integration-tests/logbacked-peers-test.js
@@ -2,6 +2,7 @@
 const Expector = require("./Expector");
 const { Database } = require("../tsc.out/implementation/Database.js");
 const { LogBackedStore } = require("../tsc.out/implementation/LogBackedStore.js");
+const { unlinkSync, existsSync } = require("fs");
 /*
 Logbacked1 <- Share File -> Logbacked2
                                 v
@@ -18,11 +19,15 @@ process.chdir(__dirname + "/..");
     await server.expect("listening", 10000);
     console.log("server started");
 
-    const lbstore1 = new LogBackedStore("/tmp/test_peer.store");
+    const path = "/tmp/test_peer.store";
+
+    if (existsSync(path))
+        unlinkSync(path);
+    const lbstore1 = new LogBackedStore(path);
     const instance1 = new Database(lbstore1);
     await instance1.ready;
 
-    const lbstore2 = new LogBackedStore("/tmp/test_peer.store");
+    const lbstore2 = new LogBackedStore(path);
     const instance2 = new Database(lbstore2);
     await instance2.ready;
     await instance2.connectTo(`ws://localhost:${port}`);
