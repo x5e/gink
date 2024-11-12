f88e73337af63d845b29f0495e348cc5215f6243
Darin McGill <darin.mcgill@gmail.com>
2024-11-12 01:21:48 -0500

    tweaks

--- python/setup.py
+++ python/setup.py
@@ -17,6 +17,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: Apache Software License',
     ],
     keywords='gink lmdb crdt history versioned',
