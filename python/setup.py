2b8df998934e37942364e6d5583ff71e2999eb1e
Darin McGill <darinmcgill@users.noreply.github.com>
2025-08-22 02:33:53 -0400

    misc (#464)
    * more tweaks
    
    * tweaks

--- python/setup.py
+++ python/setup.py
@@ -14,6 +14,7 @@ setup(
         'Development Status :: 4 - Beta',
         "Intended Audience :: Developers",
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
@@ -21,7 +22,7 @@ setup(
     ],
     keywords='gink lmdb crdt history versioned',
     packages=find_packages(),
-    python_requires=">=3.10, <4",
+    python_requires=">=3.9, <4",
     install_requires=[
         "wsproto",
         "sortedcontainers",
