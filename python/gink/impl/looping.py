a9efa51dfc786f0964e11f2f981a47f1b117aba8
Darin McGill <git@darinmcgill.com>
2025-12-21 13:06:52 -0500

    deal with case of connection being broken and socket reused

--- python/gink/impl/looping.py
+++ python/gink/impl/looping.py
@@ -1,4 +1,4 @@
-from typing import *
+from typing import Optional, Iterable, Set, ContextManager, cast
 from selectors import DefaultSelector, BaseSelector, EVENT_READ
 from contextlib import nullcontext
 from logging import getLogger
@@ -21,6 +21,7 @@ def loop(
     selector = selector or DefaultSelector()
     assert isinstance(selector, BaseSelector)
     registered: Set[Selectable] = set()
+    fd_mappings: dict[int, Selectable] = {}
     until_muts = None if until is None else resolve_timestamp(until)
 
     def add(_selectables: Iterable[Optional[Selectable]]):
@@ -28,10 +29,21 @@ def loop(
         assert isinstance(selector, BaseSelector)
         for selectable_ in _selectables:
             if selectable_ and selectable_ not in registered:
-                if hasattr(selectable_, "get_selectables"):
-                    add(selectable_.get_selectables())
+                if selectable_.fileno() in fd_mappings:
+                    # this should only happen if a connection is closed without finishing
+                    # and a new one opened that reuses the same fileno.
+                    _logger.warning("fileno %s already registered, replacing", selectable_.fileno())
+                    currently_registered = fd_mappings.pop(selectable_.fileno())
+                    if currently_registered is selectable_:
+                        raise RuntimeError("unexpected not in registered: %s", selectable_)
+                    registered.discard(currently_registered)
+                    selector.unregister(currently_registered)
+                    # maybe we should close currently_registered? It might close reused fd.
                 selector.register(selectable_, EVENT_READ)
                 registered.add(selectable_)
+                fd_mappings[selectable_.fileno()] = selectable_
+                if hasattr(selectable_, "get_selectables"):
+                    add(getattr(selectable_, "get_selectables")())
 
     add(selectables)
     context_manager = context_manager or nullcontext()
@@ -50,6 +62,8 @@ def loop(
                 except Finished as finished:
                     _logger.debug("removing connection %s", finished)
                     selector.unregister(selectable)
+                    assert selectable.fileno() in fd_mappings, "missing fileno in fd_mappings"
+                    fd_mappings.pop(selectable.fileno(), None)
                     selectable.close()
                     registered.remove(selectable)
                     if selectable is context_manager:
@@ -57,6 +71,6 @@ def loop(
             else:
                 for selectable in registered:
                     if hasattr(selectable, "on_timeout"):
-                        selectable.on_timeout()
+                        getattr(selectable, "on_timeout")()
         for selectable in registered:
             selector.unregister(selectable)
